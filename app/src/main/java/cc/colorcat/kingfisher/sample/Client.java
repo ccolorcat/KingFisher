6da13657966c4f722cef20a2f868fb08d492a151
xxchen <xxcheng@dolphin-x.info>
2019-12-25 09:43:19 +0800

    [mod] mod sample code.

--- app/src/main/java/cc/colorcat/kingfisher/sample/Client.java
+++ app/src/main/java/cc/colorcat/kingfisher/sample/Client.java
@@ -7,7 +7,7 @@ import com.google.gson.GsonBuilder;
 
 import cc.colorcat.kingfisher.core.KingFisher;
 import cc.colorcat.kingfisher.parser.gson.GsonParserFactory;
-import cc.colorcat.netbird.GenericPlatform;
+import cc.colorcat.netbird.Level;
 import cc.colorcat.netbird.NetBird;
 import cc.colorcat.netbird.Platform;
 import cc.colorcat.netbird.android.AndroidPlatform;
@@ -22,12 +22,14 @@ public class Client extends Application {
         Platform platform = new AndroidPlatform();
         NetBird defaultClient = new NetBird.Builder("https://api.github.com/")
                 .platform(platform)
+                .logLevel(Level.VERBOSE)
                 .addTailInterceptor(new JsonLoggingTailInterceptor())
                 .enableGzip(true)
                 .build();
 
         NetBird moocClient = new NetBird.Builder("http://www.imooc.com/")
                 .platform(platform)
+                .logLevel(Level.VERBOSE)
                 .addTailInterceptor(new JsonLoggingTailInterceptor(true))
                 .enableGzip(true)
                 .build();
